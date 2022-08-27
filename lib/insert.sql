c3031f31edaf1aea11d3c1fcdc0bf01fdfa6306e
rickie <rickiemaweu@gmail.com>
2022-08-28 00:11:36 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Mr.Chocolate",5,"M", "Brown", "High", true);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Rowdy",7,"F", "Brown-black", "Medium", true);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Tabitha",3,"M", "Brown-white", "High", true);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Seargent Brown",13,"M", "Black", "Low", false);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Melissa",1,"F", "White", "Medium", true);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Grinch",10,"M", "Brown", "High", true);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES("Wendy",2,"F", "Black", "Low", true);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES(NULL,7,"F", "Black-white", "High", true);
